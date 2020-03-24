07f51d2f21a007f0ceaec604b0d075e8eadb4c3b
Thomas <thomas.chatard@etu.unilim.fr>
2020-03-25 00:25:47 +0100

    Ajouts taches derniere page

--- app/src/main/java/com/example/tacheroulette/RoueHasard.java
+++ app/src/main/java/com/example/tacheroulette/RoueHasard.java
@@ -26,13 +26,21 @@ public class RoueHasard extends Activity {
         setContentView(R.layout.rouehasard);
 
         Intent intent = getIntent();
-        String[] str = new String[500];
+        String[] tabnoms = new String[500];
         if (intent.hasExtra("noms")){
-            str = intent.getStringArrayExtra("noms");
+            tabnoms = intent.getStringArrayExtra("noms");
         }
 
+        String[] tabtaches = new String[500];
+        if (intent.hasExtra("noms")){
+            tabtaches = intent.getStringArrayExtra("taches");
+        }
+
+        final TextView tv = findViewById(R.id.tv);
+        tv.setText(tabtaches[0]);
+
         int position = 0;
-        while (str[position] != null){
+        while (tabnoms[position] != null){
             position = position + 1;
         }
 
@@ -48,75 +56,10 @@ public class RoueHasard extends Activity {
 
         for(int i = 0; i < position; i++){
             LuckyItem luckyItem1 = new LuckyItem();
-            luckyItem1.topText = str[i];
+            luckyItem1.topText = tabnoms[i];
             luckyItem1.color = 0xffFFF3E0;
             data.add(luckyItem1);
         }
-
-        /*
-        LuckyItem luckyItem2 = new LuckyItem();
-        luckyItem2.topText = "200";
-        luckyItem2.color = 0xffFFE0B2;
-        data.add(luckyItem2);
-
-        LuckyItem luckyItem3 = new LuckyItem();
-        luckyItem3.topText = "300";
-        luckyItem3.color = 0xffFFCC80;
-        data.add(luckyItem3);
-
-        //////////////////
-        LuckyItem luckyItem4 = new LuckyItem();
-        luckyItem4.topText = "400";
-        luckyItem4.color = 0xffFFF3E0;
-        data.add(luckyItem4);
-
-        LuckyItem luckyItem5 = new LuckyItem();
-        luckyItem5.topText = "500";
-        luckyItem5.color = 0xffFFE0B2;
-        data.add(luckyItem5);
-
-        LuckyItem luckyItem6 = new LuckyItem();
-        luckyItem6.topText = "600";
-        luckyItem6.color = 0xffFFCC80;
-        data.add(luckyItem6);
-        //////////////////
-
-        //////////////////////
-        LuckyItem luckyItem7 = new LuckyItem();
-        luckyItem7.topText = "700";
-        luckyItem7.color = 0xffFFF3E0;
-        data.add(luckyItem7);
-
-        LuckyItem luckyItem8 = new LuckyItem();
-        luckyItem8.topText = "800";
-        luckyItem8.color = 0xffFFE0B2;
-        data.add(luckyItem8);
-
-
-        LuckyItem luckyItem9 = new LuckyItem();
-        luckyItem9.topText = "900";
-        luckyItem9.color = 0xffFFCC80;
-        data.add(luckyItem9);
-        ////////////////////////
-
-        LuckyItem luckyItem10 = new LuckyItem();
-        luckyItem10.topText = "1000";
-        luckyItem10.color = 0xffFFE0B2;
-        data.add(luckyItem10);
-
-        LuckyItem luckyItem11 = new LuckyItem();
-        luckyItem11.topText = "2000";
-        luckyItem11.color = 0xffFFE0B2;
-        data.add(luckyItem11);
-
-        LuckyItem luckyItem12 = new LuckyItem();
-        luckyItem12.topText = "3000";
-        luckyItem12.color = 0xffFFE0B2;
-        data.add(luckyItem12);
-
-        /////////////////////
-
-         */
 
         luckyWheelView.setData(data);
         luckyWheelView.setRound(5);
--- app/src/main/java/com/example/tacheroulette/RoueHasardTaches.java
+++ app/src/main/java/com/example/tacheroulette/RoueHasardTaches.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class RoueHasardTaches extends AppCompatActivity {
 
@@ -22,15 +25,28 @@ public class RoueHasardTaches extends AppCompatActivity {
             str = intent.getStringArrayExtra("noms");
         }
 
-        final TextView tv = findViewById(R.id.tv);
-        tv.setText(str[0]);
-
         final Button suivant = findViewById(R.id.suivant);
+        final Button ajouter = findViewById(R.id.ajouter);
+        final ArrayList<String> taches = new ArrayList<String>();
+        final String[] array = new String[500];
+
+        ajouter.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                final EditText saisie = (EditText) findViewById(R.id.et1);
+                taches.add(saisie.getText().toString());
+                saisie.setText(" ");
+            }
+        });
+
         final String[] finalStr = str;
         suivant.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
+                for (int i = 0; i <= taches.size()-1; i++){
+                    array[i] = taches.get(i);
+                }
                 Intent intent = new Intent(RoueHasardTaches.this, RoueHasard.class);
                 intent.putExtra("noms", finalStr);
+                intent.putExtra("taches", array);
                 RoueHasardTaches.this.startActivity(intent);
             }
         });
